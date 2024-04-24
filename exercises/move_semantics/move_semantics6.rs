fd8a64eb5bcc02af4ae3fae9efc9104c5a2750cf
scat <suzuki1968@protonmail.com>
2024-04-24 16:39:51 +0800

    move_semantics

--- exercises/move_semantics/move_semantics6.rs
+++ exercises/move_semantics/move_semantics6.rs
@@ -10,19 +10,19 @@
 fn main() {
     let data = "Rust is great!".to_string();
 
-    get_char(data);
+    get_char(&data); //有references ->函数不接管ownership；反之则接管
 
-    string_uppercase(&data);
+    string_uppercase(data);
 }
 
 // Should not take ownership
-fn get_char(data: String) -> char {
+fn get_char(data: &String) -> char {
     data.chars().last().unwrap()
 }
 
 // Should take ownership
-fn string_uppercase(mut data: &String) {
-    data = &data.to_uppercase();
+fn string_uppercase(mut data: String) { //记得删除参数和函数内部的ref
+    data = data.to_uppercase();
 
     println!("{}", data);
 }
