bd182dd9923fb00bc56faa56351932b4029a60f2
scat <suzuki1968@protonmail.com>
2024-04-25 20:04:51 +0800

    modules

--- exercises/modules/modules2.rs
+++ exercises/modules/modules2.rs
@@ -7,12 +7,10 @@
 // Execute `rustlings hint modules2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 mod delicious_snacks {
     // TODO: Fix these use statements
-    use self::fruits::PEAR as ???
-    use self::veggies::CUCUMBER as ???
+    pub use self::fruits::PEAR as fruit; //hint by other's github src, 需要修改这里为pub
+    pub use self::veggies::CUCUMBER as veggie;
 
     mod fruits {
         pub const PEAR: &'static str = "Pear";
