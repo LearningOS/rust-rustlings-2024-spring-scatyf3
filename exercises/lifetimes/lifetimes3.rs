003c2fcc95e14cb384f230ee52526480be9f533a
scat <suzuki1968@protonmail.com>
2024-04-27 16:19:47 +0800

    lifetime

--- exercises/lifetimes/lifetimes3.rs
+++ exercises/lifetimes/lifetimes3.rs
@@ -5,16 +5,17 @@
 // Execute `rustlings hint lifetimes3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
-struct Book {
-    author: &str,
-    title: &str,
+struct Book<'a> { //保证引用的数据生命周期大于结构体 
+    author: &'a str,
+    title: &'a str,
 }
 
 fn main() {
+
     let name = String::from("Jill Smith");
     let title = String::from("Fish Flying");
+
     let book = Book { author: &name, title: &title };
 
     println!("{} by {}", book.title, book.author);
