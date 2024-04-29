fcf4eb9ab90500207c3d4c86f1243a0f11b0ed75
scat <suzuki1968@protonmail.com>
2024-04-29 13:46:01 +0800

    conversions

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,40 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len()==0{
+            return Person{name:"John".to_string(),age:30};
+        }
+        let splitted: Vec<&str> = s.split(",").collect();
+        if splitted.len()!=2{
+            return Person{name:"John".to_string(),age:30};
+        }
+        if splitted[0]==""{
+            return Person{name:"John".to_string(),age:30};
+        }
+        let a = splitted[1].parse::<usize>();
+        
+
+match a {
+    Ok(age) => {
+        // a 成功解析为 usize
+        let person = Person {
+            name: splitted[0].to_string(),
+            age: age,
+        };
+        person
+    }
+    Err(_) => {
+        // a 解析失败
+        let person = Person {
+            name: "John".to_string(),
+            age: 30,
+        };
+        person
+    }
+}
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,7 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -51,14 +50,42 @@ enum ParsePersonError {
 
 impl FromStr for Person {
     type Err = ParsePersonError;
-    fn from_str(s: &str) -> Result<Person, Self::Err> {
+    fn from_str(s: &str) -> Result<Person, Self::Err> { //增加error包装到Result里作为返回值
+        if s.is_empty() {
+            return Err(ParsePersonError::Empty);
+        }
+
+        let splitted: Vec<&str> = s.split(",").collect();
+        if splitted.len() != 2 {
+            return Err(ParsePersonError::BadLen);
+        }
+
+        let name = splitted[0].trim();
+        if name.is_empty() {
+            return Err(ParsePersonError::NoName);
+        }
+
+        let age_result = splitted[1].trim().parse::<usize>();
+        if let Err(err) = age_result {
+            return Err(ParsePersonError::ParseInt(err));
+        }
+        let age = age_result.unwrap();
+
+        Ok(Person {
+            name: name.to_string(),
+            age: age,
+        })
     }
 }
+
+
+
 
 fn main() {
     let p = "Mark,20".parse::<Person>().unwrap();
     println!("{:?}", p);
 }
+
 
 #[cfg(test)]
 mod tests {
